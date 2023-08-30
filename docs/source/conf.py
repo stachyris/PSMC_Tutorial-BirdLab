cb1d60ed65cc1cf05e26c4871857465356201fe4
Vinay K L <61734552+stachyris@users.noreply.github.com>
2023-08-30 09:23:16 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PSMC Tutorial'
+copyright = '2023, Vinay'
+author = 'Vinay K L'
 
 release = '0.1'
 version = '0.1.0'
